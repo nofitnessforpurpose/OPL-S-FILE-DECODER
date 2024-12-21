fa516148f13ea3120282d48451b448560e241873
nofitnessforpurpose <nofitnessforpurpose@gmail.com>
2024-12-21 10:10:34 +0000

    Code commenting

--- code/beep.asm
+++ code/beep.asm
@@ -8,14 +8,19 @@
 /*								*/
 /****************************************************************/
 ;
-#include "osvars.inc"
-#include "oseror.inc"
-#include "oshead.inc"			
-#include "swi.inc"
-
-	.org 000
-	.radix 10
+; Standard includes bundled with PSION ORGANISER II CROSS ASSEMBLER
 ;
+#include "osvars.inc"		; Contains the MKII operating system variables.
+#include "oseror.inc"		; Contains all the operating system errors.
+#include "oshead.inc"		; Contains constants and macros for the MKII operating system.
+#include "swi.inc"		; Contains the MK2 SWI's. (Software Interrupt Vectors)
+
+; Default config options
+;
+	.org 000		; Sets address assembly will start (or continue)
+	.radix 10		; Sets the default base used by the assembler
+;
+; Code start
 ;
 start:
         	OS BZ$ALRM      ; Call the ROM routine that sounds an alarm call.
